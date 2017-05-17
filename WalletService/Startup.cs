b666a97812a1c0a48e90c7171778f27f78b9f21f
andrei <alloandrei@yahoo.com>
2017-05-17 22:48:13 +0200

    setup of owin and autofac

---
+++ WalletService/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+using System.Reflection;
+using System.Web.Http;
+using Microsoft.Owin.Host;
+
+[assembly: OwinStartupAttribute(typeof(WalletService.Startup))]
+namespace WalletService
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            // Register dependencies, then...
+            var container = builder.Build();
+
+            // Register the Autofac middleware FIRST. This also adds
+            // Autofac-injected middleware registered with the container.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var config = CreateHttpConfiguration();
+            
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            app.UseWebApi(config);
+        }
+
+        public static HttpConfiguration CreateHttpConfiguration()
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            return httpConfiguration;
+        }
+    }
+}
